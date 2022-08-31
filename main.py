a1dce97c14321131b1b22a60106049e7a9bea352
Tri Hoang <tri742001@gmail.com>
2022-08-31 08:56:29 +0700

    modify the UI

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 start = dt.datetime(2016, 1, 2)
 end = dt.datetime(2022, 8, 29)
 
-stock_name=input("Enter the stock symbol of your firm (capitalized): ")
+stock_name=input("Enter the stock symbol of your firm: ")
 stock_name = stock_name.upper()
 
 stock = web.DataReader(stock_name, 'yahoo', start, end)
@@ -66,14 +66,16 @@ stock_data['Buy_Signal_Price'] = signal[0]
 stock_data['Sell_Signal_Price']= signal[1]
 
 plt.figure(figsize=(15,5))
-plt.plot(stock_data['Stock'],label = 'FB', alpha = 0.30)
+plt.plot(stock_data['Stock'],label = stock_name, alpha = 0.30)
 plt.plot(stock_data['SMA30'], label = 'SMA30', alpha = 0.30)
 plt.plot(stock_data['SMA100'], label = 'SMA100', alpha = 0.30)
+plt.xlabel("Jan 2016 - Aug 2022")
+plt.ylabel("Prices in USD")
 plt.scatter(stock_data.index,stock_data['Buy_Signal_Price'], label = 'Buy', marker = '^', color = 'green')
 plt.scatter(stock_data.index,stock_data['Sell_Signal_Price'], label = 'Sell', marker = 'v', color = 'red')
 plt.legend(loc='upper left')
 plt.show()
-print(stock_data.index)
+print(stock_data)
 
 
 

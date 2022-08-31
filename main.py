88399d7f1cc4e44925827f84255192b64ab33370
Tri Hoang <tri742001@gmail.com>
2022-08-31 18:51:14 +0700

    change the SMA rate

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ stock_name=input("Enter the stock symbol of your firm: ")
 stock_name = stock_name.upper()
 
 stock = web.DataReader(stock_name, 'yahoo', start, end)
-SMA30=pd.DataFrame()
-SMA30['Adj Close']=stock['Adj Close'].rolling(30).mean()
+SMA50=pd.DataFrame()
+SMA50['Adj Close']=stock['Adj Close'].rolling(50).mean()
 
-SMA100=pd.DataFrame()
-SMA100['Adj Close']=stock['Adj Close'].rolling(100).mean()
+SMA200=pd.DataFrame()
+SMA200['Adj Close']=stock['Adj Close'].rolling(200).mean()
 
 plt.figure(figsize=(15, 5))
 plt.plot(stock['Adj Close'], label=stock_name)
-plt.plot(SMA30['Adj Close'], label="SMA30")
-plt.plot(SMA100['Adj Close'], label="SMA100")
+plt.plot(SMA50['Adj Close'], label="SMA50")
+plt.plot(SMA200['Adj Close'], label="SMA100")
 plt.xlabel("Jan 2016- Aug 2022")
 plt.ylabel("Adj Close Prices in USD")
 plt.legend(loc="upper left")
@@ -29,8 +29,8 @@ plt.show()
 
 stock_data = pd.DataFrame()
 stock_data['Stock'] = stock['Adj Close']
-stock_data['SMA30'] = SMA30['Adj Close']
-stock_data['SMA100'] = SMA100['Adj Close']
+stock_data['SMA50'] = SMA50['Adj Close']
+stock_data['SMA200'] = SMA200['Adj Close']
 
 #function to create signals for buying and selling
 def signalStock(data):
@@ -39,7 +39,7 @@ def signalStock(data):
     flag = -1 #if flag is 1, a buy is recently conducted, or if flag is 0, a sell is recently conducted. Use -1 to start
 
     for i in range(len(data)):
-        if data['SMA30'][i]> data['SMA100'][i]: #when short term SMA crosses above long term SMA, BUY
+        if data['SMA50'][i]> data['SMA200'][i]: #when short term SMA crosses above long term SMA, BUY
             if flag!=1:
                 signalBuyPrice.append(stock_data['Stock'][i])
                 signalSellPrice.append(np.nan)
@@ -47,7 +47,7 @@ def signalStock(data):
             else:
                 signalBuyPrice.append(np.nan)
                 signalSellPrice.append(np.nan)
-        elif data['SMA30'][i] < data['SMA100'][i]: #when short term SMA crosses above long term SMA, BUY
+        elif data['SMA50'][i] < data['SMA200'][i]: #when short term SMA crosses above long term SMA, BUY
             if flag!=0:
                 signalSellPrice.append(stock_data['Stock'][i])
                 signalBuyPrice.append(np.nan)
@@ -67,8 +67,8 @@ stock_data['Sell_Signal_Price']= signal[1]
 
 plt.figure(figsize=(15,5))
 plt.plot(stock_data['Stock'],label = stock_name, alpha = 0.30)
-plt.plot(stock_data['SMA30'], label = 'SMA30', alpha = 0.30)
-plt.plot(stock_data['SMA100'], label = 'SMA100', alpha = 0.30)
+plt.plot(stock_data['SMA50'], label = 'SMA50', alpha = 0.30)
+plt.plot(stock_data['SMA200'], label = 'SMA200', alpha = 0.30)
 plt.xlabel("Jan 2016 - Aug 2022")
 plt.ylabel("Prices in USD")
 plt.scatter(stock_data.index,stock_data['Buy_Signal_Price'], label = 'Buy', marker = '^', color = 'green')
